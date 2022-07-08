1dea0ba653ae4774c0ce3f446b081623d0c9ec52
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2022-07-07 22:53:13 -0300

    Pom configurado corretamente para o heroku, e implementações no propeties

---
+++ magic/src/main/java/br/com/magic/magicards/AplicationMagic.java
@@ -0,0 +1,17 @@
+package br.com.magic.magicards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AplicationMagic {
+
+
+			public static void main(String[] args) {
+				SpringApplication.run(AplicationMagic.class, args);
+			}
+	}
+
+
+
+
+
---
+++ magic/src/test/java/br/com/magic/magicards/AplicationMagicTests.java
@@ -0,0 +1,13 @@
+package br.com.magic.magicards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationMagicTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
